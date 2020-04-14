f3c285902d86c9d6daddc8af3a092993600ff2d0
tawfiq_amro <tawfeekamr91@gmail.com>
2020-04-14 05:39:39 +0300

    add new pages

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,14 @@
 import "../css/aos.css"
 import "../css/hover.css"
+import "../css/plyr-pink.css"
 import "../css/master.css"
 
 import '../js/vendor/modernizr-3.8.0.min'
 import '../js/vendor/aos'
+import Plyr from 'plyr';
+
+const player = new Plyr('#player');
+
 
 AOS.init();
 
