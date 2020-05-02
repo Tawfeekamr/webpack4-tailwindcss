700339509f9d8085c32c735409e1abca18125882
tawfiq_amro <tawfeekamr91@gmail.com>
2020-05-02 05:12:10 +0300

    init webpack4 and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,38 @@
 module.exports = {
   theme: {
     fontFamily: {
-      light: ['Cocon Next Arabic Light', 'sans-serif'],
-      body: ['Cocon Next Arabic Regular', 'sans-serif'],
-      bold: ['Cocon Next Arabic Bold', 'sans-serif'],
+      // light: ['Exanple font Light', 'sans-serif'],
+      // body: ['Exanple font Regular', 'sans-serif'],
+      // bold: ['Exanple font Bold', 'sans-serif'],
     },
     colors: {
-      pink: '#FF0058',
-      yellow: '#FAD848',
-      gray: '#535B6C',
-      light: '#AAB3C2',
-      slight: '#E7EBF2',
-      white: '#fff',
-      red: '#F50000',
-      softblack: '#0A0E20'
+      // Example for add new colors
+      // pink: '#FF0058',
+      // yellow: '#FAD848',
+      // gray: '#535B6C',
+      // light: '#AAB3C2',
+      // slight: '#E7EBF2',
+      // white: '#fff',
+      // red: '#F50000',
+      // softblack: '#0A0E20'
     },
     container: {
       center: true,
     },
     extend: {
+      // Extend will extend tailwind default data, you can use it also with fontFamily, fontSize, colors ...etc
       screens: {
-        '2xl': '1400px',
-        '3xl': '1600px',
-        '4xl': '1900px',
+        // Example for add new breakpoints
+        // '2xl': '1400px',
+        // '3xl': '1600px',
+        // '4xl': '1900px',
       },
       fontSize: {
-        '8xl': '6rem',
-      }
+        // Example for add new fontsizes
+        // '8xl': '6rem',
+      },
     },
   },
-  variants: {
-  borderColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
-},
+  variants: {},
   plugins: [],
-}
+};
